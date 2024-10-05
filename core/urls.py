59371f779548f3d7cf94d1099ad8bfe5ab7a1901
Austin Gabriel Pardosi <gabrielpardosi26@gmail.com>
2024-10-05 14:31:53 +0700

    [Feat] Add text-to-speech implementation

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, transcribe_audio, text_to_speech_file
 
 urlpatterns = [
-    path('', home, name='home'),   
+    path('', home, name='home'),  
+    path('transcribe/', transcribe_audio, name='transcribe_audio'),
+    path('text-to-speech/', text_to_speech_file, name='text_to_speech'),
 ]
